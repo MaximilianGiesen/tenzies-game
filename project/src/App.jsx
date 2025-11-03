26c66aff0cd44b1fafa1196edac85cc728384602
MaximilianGcodes <maximilian.giesen@denkwerk.com>
2025-11-03 15:28:42 +0100

    feat: added hardcoded dice with value props and added main styling

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,10 +1,42 @@
 import React from 'react';
+import Die from "./components/Die.jsx";
 import './App.css';
 
 export default function App() {
     return (
         <main>
-            <h1>Hello World</h1>
+            <div className="dice-container">
+                <Die
+                    value="1"
+                />
+                <Die
+                    value="2"
+                />
+                <Die
+                    value="3"
+                />
+                <Die
+                    value="4"
+                />
+                <Die
+                    value="5"
+                />
+                <Die
+                    value="6"
+                />
+                <Die
+                    value="1"
+                />
+                <Die
+                    value="2"
+                />
+                <Die
+                    value="3"
+                />
+                <Die
+                    value="4"
+                />
+            </div>
         </main>
     )
 }
