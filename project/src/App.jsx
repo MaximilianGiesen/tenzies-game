422d98febf13acd317c7554b5aedbcbb3b2f9ad1
MaximilianGcodes <maximilian.giesen@denkwerk.com>
2025-11-05 14:19:07 +0100

    feat: Added function to generate random number

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -2,7 +2,21 @@ import React from 'react';
 import Die from "./components/Die.jsx";
 import './App.css';
 
-export default function App() {
+export default function App(max) {
+
+    const generateAllNewDice = () => {
+        let min = 1;
+        let max = 6;
+        let random = [];
+
+        for (let i = min; i <= 10; i++) {
+            let n = Math.floor(Math.random() * (max - min + 1)) + min; random.push(n);
+        }
+
+        return random;
+
+    }
+
     return (
         <main>
             <div className="dice-container">
